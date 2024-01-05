d8926b93885d77d4f116b3fc6b45402d717b36bf
WhiredPlanck <whiredplanck@outlook.com>
2024-01-05 11:33:25 +0800

    feat: set fetch depth as 1 when libgit2 version no less than v1.7.0

---
+++ src/git.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <git2.h>
+
+#ifdef LIBGIT2_VER_MINOR
+# define CHECK_LIBGIT2_VERSION(MAJOR, MINOR) \
+  ((LIBGIT2_VER_MAJOR == (MAJOR) && LIBGIT2_VER_MINOR >= (MINOR)) || \
+   LIBGIT2_VER_MAJOR > (MAJOR))
+#else /* ! defined(LIBGIT2_VER_MINOR) */
+# define CHECK_LIBGIT2_VERSION(MAJOR, MINOR) 0
+#endif
